086bc0ab2da5470582cb4daaebefa9ca33790deb
Ibrahem Mahmoud <aprahemmahmoud3@gmail.com>
2025-09-04 14:47:53 +0300

    initial commit

---
+++ 1~17/js/main.js
@@ -0,0 +1,66 @@
+// console.log("%cElzero" , "color: blue;font-size: 80px;font-weight: bold;text-align: center;font-family: Arial")
+// document.write(`<h1 style="color: blue;font-size: 80px;font-weight: bold;text-align: center;font-family: Arial;">Elzero</h1>`)
+
+// document.write("<h1 style=\"color: blue;font-size: 80px;font-weight: bold;text-align: center;font-family: Arial;\">Elzero</h1>")
+
+
+
+
+// console.log("%cElzero %cweb %cschool" , "color:red; font-size:40px;" , "font-weight: bold; font-size:40px; color:green;" , "background:blue; font-size:40px;");
+
+
+
+// console.group("group1");
+// console.log("message");
+// console.log("message");
+// console.group("group child");
+// console.log("message");
+// console.log("message");
+// console.groupEnd();
+// console.group("group child grand");
+// console.log("message");
+// console.log("message");
+// console.groupEnd();
+// console.groupEnd();
+// console.group("group 2");
+// console.log("message");
+// console.log("message");
+
+
+
+// console.table(["Elzero","Ahmed","sameh","gamal","Aya"]);
+
+// window.Error("hh")
+
+// window.close();
+
+//console.log("Iam In Console");
+//document.write("Iam In Page");
+
+// let num1 = 10, num2 =20 ;
+
+// console.log(num1 + "" + num2);
+// console.log(typeof(num1 + "" + num2));
+
+// console.log(`${num1}${num2}`);
+// console.log(typeof(`${num1}${num2}`));
+
+// console.log(num1+"\n"+num2);
+
+// console.log(`
+//     ${num1} 
+//     ${num2}`);
+
+// console.log(elzero.innerHTML); // object
+// console.log(typeof elzero); // object
+
+
+// console.log("`I'm In \n \\\\ \n Love \\\\ \"\"\" \'\'\' \n ++ with ++ \n \\\"\"\"\\\"\"\" \n \"\"JavaScript\"\"``")
+
+let a = 21;
+let b = 20;
+
+console.log("_"+b+("_"+ b +""+a).repeat(3)+"_"+a); // _21_2021_2021_2021_20_
+
+
+
