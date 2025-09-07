81a1a6b78d8c8fdb5874c27a18c60f09d4720095
Ibrahem Mahmoud <aprahemmahmoud3@gmail.com>
2025-09-07 14:33:28 +0300

    assignments 23~30 are done

---
+++ 23~30/main.js
@@ -0,0 +1,67 @@
+// Examples
+// console.log(100_000); // 100000
+// console.log(100000); // 100000
+// console.log(5e4 + 5e4); // 100000
+
+// // Your Solutions
+// console.log(10 ** 5); // 100000
+// console.log(10 * 10 * 10 *10 *10); // 100000
+// console.log(10e4); // 100000
+// console.log(100000.000); // 100000
+// console.log(100*1000); // 100000
+// console.log(Math.pow(10,5)); // 100000
+// console.log(Number(100000.23423.toFixed())); // 100000
+// console.log(); // 100000
+// console.log(); // 100000
+// console.log(); // 100000
+
+
+
+// console.log(Number.MAX_SAFE_INTEGER ); // 9007199254740991
+
+
+
+// console.log(Number.MAX_SAFE_INTEGER); // 16
+
+// let myVar = "100.56789 Views";
+
+// console.log( parseInt(myVar)); // 100
+// console.log(Number(parseFloat(myVar).toFixed(2))); // 100.57
+
+// let num = 10;
+
+// console.log(Number.isInteger(num) + Number.isInteger(num)); // 2
+
+
+// let flt = 10.4;
+
+// console.log( Number(flt.toFixed())); // 10
+// console.log(parseInt(flt)); // 10
+// console.log(Math.round(flt)); // 10
+// console.log(Math.floor(flt)); // 10
+// console.log(Math.trunc(flt) ); // 10
+
+
+// console.log(Math.floor(Math.random() * 5)); // 0 || 1 || 2 || 3 || 4
+
+
+// ///////////////////////
+
+// let userName = "Elzero";
+// console.log(userName[0].toLocaleLowerCase()); // e
+// console.log(userName.slice(0,1).toLocaleLowerCase()); // e
+// console.log(userName.split("lzero").toString().toLocaleLowerCase()); // e
+// console.log(userName.substr(0,1).toString().toLocaleLowerCase()); // e
+// console.log(userName.charAt(0).toLocaleLowerCase()); // e
+// console.log(userName.substring(0,1).toLocaleLowerCase()); // e
+// console.log(userName[0].repeat(3).toLocaleLowerCase()); // eee
+
+
+// let word = "Elzero";
+// let letterZ = "z";
+// let letterE = "e";
+// let letterO = "O";
+
+// console.log(word.includes(letterZ)); // True
+// console.log(word.toLocaleLowerCase().startsWith(letterE)); // True
+// console.log(word.toUpperCase().endsWith(letterO)); // True
